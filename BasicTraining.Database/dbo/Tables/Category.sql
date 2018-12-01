40b69ba3aa4475fd1ef51dcd984d28999f43d92c
Luke Vo <datvm@outlook.com>
2018-12-01 09:06:21 +0700

    Added Database.

---
+++ BasicTraining.Database/dbo/Tables/Category.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (100) NOT NULL,
+    [Slug] VARCHAR (100)  NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Category_Slug]
+    ON [dbo].[Category]([Slug] ASC);
+
---
+++ BasicTraining.Database/dbo/Tables/Product.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [CategoryId]  INT            NOT NULL,
+    [Name]        NVARCHAR (100) NOT NULL,
+    [Slug]        VARCHAR (100)  NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Product_Category] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Product_ByCategory]
+    ON [dbo].[Product]([CategoryId] ASC);
+
